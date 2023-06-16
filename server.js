778d43ad7ace5ce0da82337ac8fe31496dbdc546
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-06-16 09:35:43 +0200

    generated testdata

--- server.js
+++ server.js
@@ -3,3 +3,32 @@ import swaggerUi from 'swagger-ui-express'
 
 const app = express()
 app.use(json())
+
+//generated with codium ai
+const TODOSTESTDATA = [
+	{
+		'title': 'Buy groceries',
+		'createdDate': '01.01.22',
+		'completedDate': null
+	},
+	{
+		'title': 'Clean the house',
+		'createdDate': '31.12.21',
+		'completedDate': '01.01.22'
+	},
+	{
+		'title': 'Finish project',
+		'createdDate': '02.01.22',
+		'completedDate': null
+	},
+	{
+		'title': 'Call mom',
+		'createdDate': '03.01.22',
+		'completedDate': '03.01.22'
+	},
+	{
+		'title': 'Go for a run',
+		'createdDate': '04.01.22',
+		'completedDate': null
+	}
+]
